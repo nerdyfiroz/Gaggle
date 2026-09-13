1f400d3240bda9e072282da5a47252f37927caa0
Md. Firoz Kabir <arafatkabir34@gmail.com>
2026-09-13 16:54:28 +0600

    chore: update project branding and config logic
    - Update project tagline and blockchain in database
    - Add condition to prevent tagline overwrite in UI

--- database/seed.postgres.sql
+++ database/seed.postgres.sql
@@ -1,5 +1,5 @@
 INSERT INTO settings(setting_group,setting_key,setting_value) VALUES
-('general','project_name','Gaggle'),('general','project_tagline','The flock is coming.'),('general','blockchain','Ethereum'),('general','wl_spots','100'),('general','twitter_url','https://x.com/itzGaggle'),
+('general','project_name','Gaggle'),('general','project_tagline','The GAGGLE is coming'),('general','blockchain','Robinhood'),('general','wl_spots','100'),('general','twitter_url','https://x.com/itzGaggle'),
 ('application','applications_enabled','1'),('application','ip_application_limit','100'),('application','rate_limit_window_minutes','10'),('application','rate_limit_max_requests','5'),('application','duplicate_wallet_protection','1'),('application','require_twitter','1'),('application','require_email','0'),('captcha','captcha_enabled','1'),('security','ip_blocking_enabled','1')
 ON CONFLICT(setting_group,setting_key) DO NOTHING;
 
